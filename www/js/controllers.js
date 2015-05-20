9d9905dd55777daff73553ee49010aa8f21752bf
mariapaulasaba <mariapaula.saba@nyu.edu>
2015-05-20 12:47:32 -0400

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('starter.controllers', [])
+
+.controller('ChallengesCtrl', function($scope) {
+
+})
+.controller('ColorsCtrl', function($scope) {
+
+})
+.controller('SettingsCtrl', function($scope) {
+
+});
